386444b8cb4cf237129973f660b617a3eff6cc91
李元元 <15221561334@163.com>
2020-05-14 15:23:32 +0800

    提交

---
+++ pages/Customeied/Customeied.js
@@ -0,0 +1,156 @@
+// pages/Customeied/Customeied.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listSelect: ["型号", "面板", "附件", "总览"],
+    listType: [{
+      id: "H40",
+      price: "1999￥"
+    }, {
+      id: "H60",
+      price: "2999￥"
+    }, {
+      id: "H80",
+      price: "3999￥"
+    }, {
+      id: "H100",
+      price: "4999￥"
+    }, ],
+    listPannel: ["左右耳相同", "左右耳单独定制", "自定义图案"],
+    selectedFlag: [false, false, false, false],
+    visible: false
+  },
+
+  // 展开折叠选择  
+  ontap: function (e) {
+    var index = e.currentTarget.dataset.index;
+    if (this.data.selectedFlag[index]) {
+      this.data.selectedFlag[index] = false;
+    } else {
+      this.data.selectedFlag[index] = true;
+    }
+
+    this.setData({
+      selectedFlag: this.data.selectedFlag
+    })
+  },
+
+  //弹出耳机选择框
+  showPannel: function (e) {
+    this.setData({
+      visible: true
+    });
+  },
+
+  //消失耳机选择框
+  handleCancel: function (e) {
+    this.setData({
+      visible: false
+    });
+  },
+  //点击我显示底部弹出框
+  clickme: function () {
+    this.showModal();
+  },
+
+  //显示对话框
+  showModal: function () {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  //隐藏对话框
+  hideModal: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
